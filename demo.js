3e7f0830b2c94eaa834386971170f7125aad7af1
ketian <2965873351@qq.com>
2025-07-24 17:18:23 +0800

    1.0.11

--- demo.js
+++ demo.js
@@ -1,3 +1,3 @@
-#!/usr/bin/env node  // 新增这一行，固定格式
-console.log("🎉 这是从远程仓库拉取的demo脚本");
-console.log("当前时间：", new Date().toLocaleString());
+#!/usr/bin/env node  // 这行是告诉系统用node执行这个脚本，必不可少
+// 下面是你的代码
+console.log("🎉 运行成功！这是我的demo脚本");
